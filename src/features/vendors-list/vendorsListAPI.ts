d6f66a5c1247a2e7845911e6c41665b45f29bf74
alimonapour <a.monapoor@gmail.com>
2023-07-03 12:02:34 +0430

    feat: rewrite vendors list api

--- src/features/vendors-list/vendorsListAPI.ts
+++ src/features/vendors-list/vendorsListAPI.ts
@@ -1,21 +1,19 @@
 import axios from "axios"
-import { setVendorsList, setVendorsCount } from "./vendorsListSlice"
 
-export const fetchAllVendors =
-  (currentPage: number, currentPageSize: number) => (dispatch: any) => {
-    console.log("current page and current size", currentPage, currentPageSize)
-    axios
-      .get("https://snappfood.ir/mobile/v3/restaurant/vendors-list", {
-        params: {
-          page: currentPage,
-          page_size: currentPageSize,
-          lat: 35.754,
-          long: 51.328,
-        },
-      })
-      .then((response) => {
-        dispatch(setVendorsList(response.data.data.finalResult))
-        dispatch(setVendorsCount(response.data.data.count))
-      })
-      .catch((error) => console.log(error))
+const apiUrl = "https://snappfood.ir/mobile/v3/restaurant/vendors-list"
+
+export const fetchData = async (currentPageSize: number) => {
+  try {
+    const response = await axios.get(apiUrl, {
+      params: {
+        page: 0,
+        page_size: currentPageSize,
+        lat: 35.754,
+        long: 51.328,
+      },
+    })
+    return response.data
+  } catch (error) {
+    throw new Error("Error fetching data")
   }
+}
