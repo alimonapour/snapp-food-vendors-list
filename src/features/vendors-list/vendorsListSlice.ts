0074a641980ce312257b442ab18c6872282455b3
alimonapour <a.monapoor@gmail.com>
2023-07-03 12:03:20 +0430

    feat: add create async thunk and extra reducer to vendors slice

--- src/features/vendors-list/vendorsListSlice.ts
+++ src/features/vendors-list/vendorsListSlice.ts
@@ -1,19 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { fetchData } from "./vendorsListAPI"
 
-export interface CounterState {
-  vendorsList: []
-  vendorsCount: number
-  status: "idle" | "loading" | "succeeded" | "failed"
+interface VendorType {
+  id: number
+  title: string
+  logo: string
+  backgroundImage: string
+  rate: number | string
+  deliveryType: string
+  deliveryPrice: number | string
+  description: string
 }
 
-const initialState: CounterState = {
+interface DataType {
+  type: string
+  data: VendorType
+}
+export interface VendorState {
+  vendorsList: DataType[]
+  vendorsCount: number
+  currentPageSize: number
+  status: "idle" | "loading" | "succeeded" | "failed"
+  error: string | null | undefined
+}
+
+export const fetchVendorsAsync = createAsyncThunk(
+  "vendors/fetchVendors",
+  async (currentPageSize: number) => {
+    try {
+      const response = await fetchData(currentPageSize)
+      return response.data
+    } catch (error) {
+      throw new Error("Error fetching data")
+    }
+  },
+)
+
+const initialState: VendorState = {
   vendorsList: [],
   vendorsCount: 0,
+  currentPageSize: 10,
   status: "idle",
+  error: null,
 }
 
 export const vendorsSlice = createSlice({
-  name: "vendors-list",
+  name: "vendors",
   initialState,
   reducers: {
     setVendorsList: (state, action) => {
@@ -22,6 +54,25 @@ export const vendorsSlice = createSlice({
     setVendorsCount: (state, action) => {
       state.vendorsCount = action.payload
     },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchVendorsAsync.pending, (state) => {
+        state.status = "loading"
+        state.error = null
+      })
+      .addCase(fetchVendorsAsync.fulfilled, (state, action) => {
+        state.status = "succeeded"
+        state.vendorsList = action.payload.finalResult
+        state.currentPageSize += 10
+        state.vendorsCount = action.payload.count
+        state.error = null
+      })
+      .addCase(fetchVendorsAsync.rejected, (state, action) => {
+        state.status = "failed"
+        state.error = action.error.message
+      })
   },
 })
 
