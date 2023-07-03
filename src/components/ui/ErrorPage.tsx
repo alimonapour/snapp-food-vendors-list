a8e0a1713e14daac3e53d6d3e4d58a874ff85044
alimonapour <a.monapoor@gmail.com>
2023-07-03 17:45:13 +0430

    feat: create error component and add to app

---
+++ src/components/ui/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import styles from "./ErrorPgae.module.scss"
+export const ErrorPage = () => {
+  return (
+    <div className={styles.container}>
+      <img className={styles.errorImage} />
+      <div className={styles.itemsContainer}>
+        <h1>متاسفانه مشکل فنی پیش آمده است</h1>
+        <p>به زودی برمیگردیم. می خوای با خبر بشی؟</p>
+        <input className={styles.input} placeholder="شماره موبایل" />
+        <button className={styles.btn}>
+          <h1>خبرم کن</h1>
+        </button>
+      </div>
+    </div>
+  )
+}
