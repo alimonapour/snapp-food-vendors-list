f9a0bb310e9483c222b624b53bdf8d6d46e94442
alimonapour <a.monapoor@gmail.com>
2023-07-03 17:44:32 +0430

    feat(style): add background image to home component and style

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom"
+import styles from "./Home.module.scss"
 
 export const Home = () => {
   const navigate = useNavigate()
   return (
-    <div>
-      <button onClick={() => navigate("/vendors-list")}>
-        Go to vendors list
+    <div className={styles.container}>
+      <button className={styles.btn} onClick={() => navigate("/vendors-list")}>
+        <h1>لیست رستوران ها</h1>
       </button>
     </div>
   )
