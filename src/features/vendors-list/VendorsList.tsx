db0969dee76777cf6f089769e6d914e198ce3364
alimonapour <a.monapoor@gmail.com>
2023-07-03 12:08:18 +0430

    feat: implement infinite scroll

--- src/features/vendors-list/VendorsList.tsx
+++ src/features/vendors-list/VendorsList.tsx
@@ -1,57 +1,98 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { VendorCard } from "../../components/ui/VendorCard"
 import { makeNumbersFarsi } from "../../utils/makeNumbersFarsi"
 import styles from "./VendorsList.module.scss"
-import { useLazyLoad } from "../../hooks/useLazyLoad"
-import { fetchAllVendors } from "./vendorsListAPI"
+import { fetchVendorsAsync } from "./vendorsListSlice"
 
-export const VendorsList = () => {
-  const loaderTriggerRef = useRef(null)
+export const VendorsList: React.FC = () => {
+  const observer = useRef<IntersectionObserver | null>(null)
+  const lastItemRef = useRef<HTMLDivElement>(null)
   const dispatch = useAppDispatch()
-  const { vendorsList, vendorsCount } = useAppSelector((state) => state.vendors)
-  const [vendors, setVendors] = useState<any[]>([])
-
-  const { isLastPage, currentPage, currentPageSize } = useLazyLoad(
-    vendors,
-    loaderTriggerRef.current,
-    vendorsCount,
+  const { vendorsList, status, error, currentPageSize } = useAppSelector(
+    (state) => state.vendors,
   )
 
-  useEffect(() => {
-    dispatch(fetchAllVendors(currentPage, currentPageSize))
-    setVendors(vendorsList)
-  }, [vendorsList, currentPageSize, currentPage])
+  const handleIntersection: IntersectionObserverCallback = (entries) => {
+    const target = entries[0]
+    if (target.isIntersecting && !(status === "loading")) {
+      dispatch(fetchVendorsAsync(currentPageSize + 10))
+    }
+  }
 
-  const vendorsListItems = vendors.filter((vendor) => vendor.type === "VENDOR")
-  const openVendorsCount = vendors.filter((vendor) => vendor.type === "TEXT")
+  useEffect(() => {
+    const options: IntersectionObserverInit = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    }
+
+    observer.current = new IntersectionObserver(handleIntersection, options)
+    if (lastItemRef.current) {
+      observer.current.observe(lastItemRef.current)
+    }
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect()
+      }
+    }
+  }, [dispatch, currentPageSize, status])
+
+  useEffect(() => {
+    dispatch(fetchVendorsAsync(10))
+  }, [dispatch])
+
+  const vendorsListItems = vendorsList.filter(
+    (vendor) => vendor["type"] === "VENDOR",
+  )
+
+  const openVendorsCount = vendorsList.filter(
+    (vendor) => vendor["type"] === "TEXT",
+  )
 
   return (
     <div className={styles.vendorsListContainer}>
+      {status === "loading" && <h1>loading......</h1>}
+      {status === "failed" && <h1>{error}</h1>}
       {openVendorsCount && (
         <h1 className={styles.openVendorsCount}>
-          {makeNumbersFarsi(openVendorsCount[0]?.data)}
+          {makeNumbersFarsi(openVendorsCount[0]?.data as any)}
         </h1>
       )}
-
       {vendorsListItems &&
-        vendorsListItems.map((vendor) => (
-          <VendorCard
-            key={vendor.data.id}
-            restaurantTitle={vendor.data.title}
-            logoUrl={vendor.data.logo}
-            coverPhotoUrl={vendor.data.backgroundImage}
-            vendorRate={makeNumbersFarsi(vendor.data.rate)}
-            deliveryType="اکسپرس"
-            deliveryPrice={makeNumbersFarsi("20000")}
-            description={vendor.data.description}
-          />
-        ))}
-      <div
-        ref={loaderTriggerRef}
-        style={{ height: "100px" }}
-        hidden={isLastPage}
-      ></div>
+        vendorsListItems.map((vendor, index) => {
+          if (index === vendorsListItems.length - 1) {
+            return (
+              <div key={vendor.data.id} ref={lastItemRef}>
+                <VendorCard
+                  key={vendor.data.id}
+                  restaurantTitle={vendor.data.title}
+                  logoUrl={vendor.data.logo}
+                  coverPhotoUrl={vendor.data.backgroundImage}
+                  vendorRate={makeNumbersFarsi(vendor.data.rate)}
+                  deliveryType="اکسپرس"
+                  deliveryPrice={makeNumbersFarsi("20000")}
+                  description={vendor.data.description}
+                />
+              </div>
+            )
+          }
+          return (
+            <div key={vendor.data.id}>
+              <VendorCard
+                key={vendor.data.id}
+                restaurantTitle={vendor.data.title}
+                logoUrl={vendor.data.logo}
+                coverPhotoUrl={vendor.data.backgroundImage}
+                vendorRate={makeNumbersFarsi(vendor.data.rate)}
+                deliveryType="اکسپرس"
+                deliveryPrice={makeNumbersFarsi("20000")}
+                description={vendor.data.description}
+              />
+            </div>
+          )
+        })}
     </div>
   )
 }
